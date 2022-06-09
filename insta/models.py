92bda981416256c6aec4bfaeb12ecadae81f7407
kelvinkip <kelvinkipla@gmail.com>
2022-06-09 08:24:44 +0300

    created test

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,92 @@
+from email import contentmanager
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    @classmethod
+    def search_profile(cls,search_term):
+        profile = cls.objects.filter(user__username__icontains=search_term)
+        return profile
+
+    @classmethod
+    def update_profile(cls,id,bio):
+        update = Post.objects.filter(id=id).update(bio = bio)
+        return update
+    
+    def __str__(self):
+        return self.bio
+        
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    image = CloudinaryField('image')
+    date_posted = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    def save_post(self):
+        self.save()
+        
+    def delete_post(self):
+        self.delete()
+        
+    @classmethod
+    def update_post(cls,id,post):
+        posted = Post.objects.filter(id=id).update(post = post)
+        return posted
+
+    @classmethod
+    def get_images(cls):
+        post = Post.objects.all()
+        return post
+
+    @classmethod
+    def get_image_by_id(cls):
+        post = Post.objects.filter(id=Post.id)
+        return post
+    
+    def total_likes(self):
+        return self.likes.count()
+    
+    def __str__(self):
+        return self.content
+    
+class Comment(models.Model):
+    comment = models.TextField(null=True)
+    post = models.ForeignKey(Post, related_name='comments',on_delete=models.CASCADE,null=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+    
+    def save_comment(self):
+        self.save()
+        
+    def delete_comment(self):
+        self.delete()
+        
+    @classmethod
+    def get_comment(cls):
+        comment = Comment.objects.all()
+        return comment
+    
+class Like(models.Model):
+	user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    
--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from .models import Profile,Post,Comment
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.user=User()
+        self.user.save()
+        self.profile=Profile(user=self.user,username='kate',bio='',profile_pic='')
+    def tearDown(self):
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+    def test_saveProfile(self):
+        self.profile.save_profile()
+        profile_saved = Profile.objects.all()
+        self.assertTrue(len(profile_saved) > 0)
