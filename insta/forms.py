28c5a788a0e1a22600cbbf04d466004ae1bc9486
kelvinkip <kelvinkipla@gmail.com>
2022-06-09 08:35:18 +0300

    created forms

--- insta/forms.py
+++ insta/forms.py
@@ -1,2 +1,42 @@
+from dataclasses import fields
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from . models import *
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+        
+class LoginUserForm(UserCreationForm):
+    username = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'password1']
+        
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user', 'date_posted']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+        
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'date_posted']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+        
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['user', 'date_posted','post']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+        
 
-]
