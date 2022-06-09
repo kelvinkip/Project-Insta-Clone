5aaa87afe7f5350488a059c3ca06d129c5e16019
kelvinkip <kelvinkipla@gmail.com>
2022-06-10 00:19:22 +0300

    heroku

--- manage.py
+++ manage.py
@@ -11,8 +11,7 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
+          
           
         ) from exc
     execute_from_command_line(sys.argv)
