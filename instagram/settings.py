407867fa3b90d01a981ecd0e615d91f870e5089c
kelvinkip <kelvinkipla@gmail.com>
2022-06-10 19:11:09 +0300

    heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -181,7 +181,6 @@ import dj_database_url
 from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 # Application definition
 INSTALLED_APPS = [
